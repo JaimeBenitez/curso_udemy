bb07717ebedbdd898851ceea31c4fac524ad039d
Jaime benitez <jaime.benitez@alten.es>
2023-05-04 14:18:13 +0200

    Seccion 11 - gradiente

--- 05-peliculasApp/src/screens/HomeScreen.tsx
+++ 05-peliculasApp/src/screens/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import Carousel from 'react-native-snap-carousel';
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native';
 import useMovies from '../hooks/useMovies';
 import MoviePoster from '../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import HorizontalSlider from '../components/HorizontalSlider';
+import GradientBackground from '../components/GradientBackground';
+import ImageColors from 'react-native-image-colors'
+import { getImageColors } from '../helpers/getColores';
+import { GradientContext } from '../context/GradientContext';
 
 
 
@@ -13,7 +17,20 @@ const HomeScreen = () => {
 
   const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies()
   const{ top } = useSafeAreaInsets()
-
+  const { setMainColors } = useContext( GradientContext )
+  const getPosterColor = async( index: number ) => {
+    const movie = nowPlaying[index]
+    const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`
+    const [primary = 'green', secondary = 'orange' ] = await getImageColors( uri )
+    setMainColors({primary, secondary})
+  }
+  //NOTA: si se realiza despues del if loading salta error
+  useEffect(()=>{
+    if( nowPlaying.length > 0 ){
+      getPosterColor(0)
+    }
+  }, [nowPlaying])
+  
   if( isLoading ){
     return(
       <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center'}}>
@@ -21,32 +38,36 @@ const HomeScreen = () => {
       </View>
     )
   }
+
   
   return (
-    <ScrollView>
-    <View style={{ marginTop: top + 20}}>
+    <GradientBackground >
+      <ScrollView>
+        <View style={{ marginTop: top + 20}}>
 
 
-      <View style={{ height: 440 }}>
+          <View style={{ height: 440 }}>
 
-      {/* Carrusel principal */}
-      <Carousel
-        data={nowPlaying}
-        renderItem={({ item })=> <MoviePoster movie={ item } />}
-        sliderWidth={ windowWidth }
-        itemWidth={ 300 }
-        inactiveSlideOpacity={ 0.9 }
-      />
+          {/* Carrusel principal */}
+          <Carousel
+            data={nowPlaying}
+            renderItem={({ item })=> <MoviePoster movie={ item } />}
+            sliderWidth={ windowWidth }
+            itemWidth={ 300 }
+            inactiveSlideOpacity={ 0.9 }
+            onSnapToItem={ index => getPosterColor( index)}
+          />
 
-      
-      </View>
-      {/* Peliculas populares */}
-      
-      <HorizontalSlider title="Popular" movies={ popular } />
-      <HorizontalSlider title="Top Rated" movies={ topRated } />
-      <HorizontalSlider title="Upcoming" movies={ upcoming } />
-    </View>
-    </ScrollView>
+          
+          </View>
+          {/* Peliculas populares */}
+          
+          <HorizontalSlider title="Popular" movies={ popular } />
+          <HorizontalSlider title="Top Rated" movies={ topRated } />
+          <HorizontalSlider title="Upcoming" movies={ upcoming } />
+        </View>
+      </ScrollView>
+    </GradientBackground>
   )
 }
 
